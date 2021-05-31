75781e7ad60b69f282bf5b4dd46dc7db3a02167e
LBruner <leandrobruner15@gmail.com>
2021-05-30 21:01:42 -0300

    Section 1 - 4

---
+++ Multiplayer RTS/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,22 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log("Connected"); Debug.Log($"there are : {numPlayers} players");
+
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log("Another player joined");
+    }
+}
