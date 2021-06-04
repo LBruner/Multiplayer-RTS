dde715c171e9a594a72cdc414a87f18d75ae2c6c
LBruner <leandrobruner15@gmail.com>
2021-06-04 18:12:55 -0300

    feat(Vídeo 14): Adds Health & collision to projectilles

--- Multiplayer RTS/Assets/Scripts/Units/UnitProjectille.cs
+++ Multiplayer RTS/Assets/Scripts/Units/UnitProjectille.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class UnitProjectille : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 20;
     [SerializeField] private float destroyAfterSeconds = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -15,6 +16,21 @@ public class UnitProjectille : NetworkBehaviour
     public override void OnStartServer()
     {
         Invoke(nameof(DestroySelf), destroyAfterSeconds);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            if (networkIdentity.connectionToClient == connectionToClient) { return; }
+        }
+
+        if(other.TryGetComponent<Health>(out Health health)){
+            health.DealDamage(damageToDeal);
+        }
+
+        DestroySelf();
     }
 
     private void DestroySelf()
